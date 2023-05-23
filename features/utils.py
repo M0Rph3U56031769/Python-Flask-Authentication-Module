4886d0d7e7cd483c9e5ce7c68fd15d4c47692230
nagyd <nagydaniel1337@gmail.com>
2023-05-24 00:23:57 +0200

    function/method docstrings

--- features/utils.py
+++ features/utils.py
@@ -27,6 +27,24 @@ from features.models import User
 
 
 def admin_required(f):
+    """
+    This function is a decorator that ensures the current user has admin privileges.
+
+    It's intended to be used to decorate Flask view functions that should only be accessible to
+    users with admin rights.
+
+    Parameters:
+    f (function): The function to be decorated.
+
+    If the current user does not have admin rights, the function aborts with a 403 HTTP status code
+    and a custom error message, and does not call the decorated function.
+
+    If the current user has admin rights, the function proceeds to call the decorated function.
+
+    Returns:
+    function: The decorated function, which is called if the current user has admin rights.
+    """
+
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if not current_user.admin:
@@ -45,4 +63,18 @@ def admin_required(f):
 
 @login_manager.user_loader
 def load_user(user_id):
+    """
+    This function retrieves a user by their user ID from the database.
+
+    It's set as a callback method for Flask-Login's user_loader. Flask-Login uses this function to
+    reload the user object from the user ID stored in the session.
+
+    Parameters:
+    user_id (str): A string representing the user ID. This function converts this string to an
+    integer before querying the database.
+
+    Returns:
+    User: The User object with the given ID, or None if no such user exists.
+    """
+
     return User.query.get(int(user_id))
